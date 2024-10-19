f1b3ea449d4fb0b860f2a81870e494597c2e93e3
KimGyeongsuuu <sk62628504@gmail.com>
2024-10-19 22:46:06 +0900

    feature :: delete board api

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	port := os.Getenv("DB_PORT")
 	database := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, database)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, database)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
