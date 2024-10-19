a6f8859d01ac418cfa1d0b1d5e4343c889c08af7
KimGyeongsuuu <sk62628504@gmail.com>
2024-10-19 22:45:39 +0900

    add :: delete board interface

--- model/board.go
+++ model/board.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Board struct {
-	BoardID int `gorm:"primaryKey;autoIncrement"`
+	BoardID uint64 `gorm:"primaryKey;autoIncrement"`
 	Title   string
 	Content string
 }
@@ -14,9 +14,11 @@ type Board struct {
 type BoardUseCase interface {
 	CreateBoard(ctx context.Context, input *input.CreateBoardInput) error
 	GetBoard(ctx context.Context) ([]Board, error)
+	DeleteBoard(ctx context.Context, boardID uint64) error
 }
 
 type BoardRepository interface {
 	CreateBoard(ctx context.Context, board *Board) error
 	FindAllBoard(ctx context.Context) ([]Board, error)
+	DeleteBoard(ctx context.Context, boardId uint64) error
 }
