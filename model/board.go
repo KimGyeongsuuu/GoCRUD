e0dba28fafa2dde966dfde6e920cca18e4c68b90
KimGyeongsuuu <sk62628504@gmail.com>
2024-10-19 12:44:49 +0900

    add :: get board interface

--- model/board.go
+++ model/board.go
@@ -6,15 +6,17 @@ import (
 )
 
 type Board struct {
-	BoardID int
+	BoardID int `gorm:"primaryKey;autoIncrement"`
 	Title   string
 	Content string
 }
 
 type BoardUseCase interface {
 	CreateBoard(ctx context.Context, input *input.CreateBoardInput) error
+	GetBoard(ctx context.Context) ([]Board, error)
 }
 
 type BoardRepository interface {
 	CreateBoard(ctx context.Context, board *Board) error
+	FindAllBoard(ctx context.Context) ([]Board, error)
 }
